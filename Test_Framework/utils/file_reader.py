340447a856953d5bc0935832253e7f22dab22097
““Qing-H” <qing894651361@163.com>
2017-08-22 18:18:30 +0800

    change to po

--- Test_Framework/utils/file_reader.py
+++ Test_Framework/utils/file_reader.py
@@ -1,6 +1,6 @@
 import yaml
 import os
-
+#from xlrd import open_workbook
 
 class YamlReader:
     def __init__(self, yaml):
@@ -17,3 +17,36 @@ class YamlReader:
         else:
             with open(self.yaml, 'rb') as f:
                 return list(yaml.safe_load_all(f))
+
+# class ExcelReader:
+#     def __init__(self, excel, sheet=0, title_line=True):
+#         if os.path.exists(excel):
+#             self.excel = excel
+#         else:
+#             raise FileNotFoundError('文件不存在！')
+#         self.sheet = sheet
+#         self.title_line = title_line
+#         self._data = list()
+#
+#     @property
+#     def data(self):
+#         if self._data:
+#             return self._data
+#         else:
+#             workbook = open_workbook(self.excel)
+#             if type(self.sheet) not in [int, str]:
+#                 raise SheetTypeError('Please pass in <type int> or <type str>, not {0}'.format(type(self.sheet)))
+#             elif type(self.sheet) == int:
+#                 s = workbook.sheet_by_index(self.sheet)
+#             else:
+#                 s = workbook.sheet_by_name(self.sheet)
+#
+#             if self.title_line:
+#                 title = s.row_values(0)
+#                 for col in range(1, s.nrows):
+#                     self._data.append(dict(zip(title, s.row_values(col))))
+#                 return self._data
+#             else:
+#                 for col in range(0, s.nrows):
+#                     self._data.append(s.row_values(col))
+#             return self._data
